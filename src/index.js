400ceb9c4b9282dcb0d0bab5357e37d787a91b3d
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-11-17 14:38:07 -0300

    Add function to create table with ToDo list. Add object creation from form

--- src/index.js
+++ src/index.js
@@ -3,3 +3,47 @@ import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
+
+function displayTasks(array) {
+  const todoDisplay = document.getElementById('todoDisplay');
+  array.forEach((object) => {
+    const tr = document.createElement('tr');
+    Object.keys(object).forEach((key) => {
+      const td = document.createElement('td');
+      if (key === 'isdone') {
+        const td = document.createElement('td');
+        const checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        td.append(checkbox);
+        tr.append(td);
+      } else {
+        td.innerText = object[key];
+        tr.append(td);
+      }
+    });
+    todoDisplay.append(tr);
+  });
+}
+
+
+const arrayOfTasks = [];
+
+const formSubmit = document.getElementById('submit-todo-form');
+formSubmit.addEventListener('click', (event) => {
+  const todoTitle = document.getElementById('todoTitle');
+  const selectProject = document.getElementById('selectProject');
+  const todoDescription = document.getElementById('todoDescription');
+  const todoDueDate = document.getElementById('todoDueDate');
+  const todoPriority = document.getElementById('todoPriority');
+
+  const newTodo = {
+    title: todoTitle.value,
+    description: todoDescription.value,
+    duedate: todoDueDate.value,
+    priority: todoPriority.checked,
+    project: selectProject.value,
+    isdone: false,
+  };
+  arrayOfTasks.push(newTodo);
+  displayTasks(arrayOfTasks);
+});
