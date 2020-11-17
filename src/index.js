21a45a442607615d9afc9b4a815ba2fbb0e5f6fd
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-11-17 18:21:34 -0300

    Add modal display basics and splitted the forms into different files.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
+import toDoForm from './todoform';
+import projectForm from './projectForm';
 
 function displayTasks(array) {
   const todoDisplay = document.getElementById('todoDisplay');
@@ -27,42 +29,63 @@ function displayTasks(array) {
 
 
 const arrayOfTasks = [];
-
-const formSubmit = document.getElementById('submit-todo-form');
-formSubmit.addEventListener('click', (event) => {
-  const todoTitle = document.getElementById('todoTitle');
-  const selectProject = document.getElementById('selectProject');
-  const todoDescription = document.getElementById('todoDescription');
-  const todoDueDate = document.getElementById('todoDueDate');
-  const todoPriority = document.getElementById('todoPriority');
-
-  const newTodo = {
-    title: todoTitle.value,
-    description: todoDescription.value,
-    duedate: todoDueDate.value,
-    priority: todoPriority.checked,
-    project: selectProject.value,
-    isdone: false,
-  };
-  arrayOfTasks.push(newTodo);
-  const form = document.getElementById('form');
-  form.innerHTML = '';
-  displayTasks(arrayOfTasks);
-});
-
-// add new projects
 const arrayProject = [];
-const submitProjectbtn = document.getElementById('submit-project-form');
-submitProjectbtn.addEventListener('click', (e) => {
-  let projectTitle = document.getElementById('projectTitle').value.toLowerCase();
-  if (!arrayProject.includes(projectTitle)) {
-    arrayProject.push(projectTitle);
-  };
-  console.log(arrayProject);
+
+
+
+
+
+const newTodoBtn = document.getElementById('newTodoFormBtn');
+newTodoBtn.addEventListener('click', (event) => {
+  const mainContainer = document.getElementById('modalContainer');
+  mainContainer.innerHTML += `${toDoForm}`;
+  const formSubmit = document.getElementById('submit-todo-form');
+
+  formSubmit.addEventListener('click', (event) => {
+    const todoTitle = document.getElementById('todoTitle');
+    const selectProject = document.getElementById('selectProject');
+    const todoDescription = document.getElementById('todoDescription');
+    const todoDueDate = document.getElementById('todoDueDate');
+    const todoPriority = document.getElementById('todoPriority');
+
+    const newTodo = {
+      title: todoTitle.value,
+      description: todoDescription.value,
+      duedate: todoDueDate.value,
+      priority: todoPriority.checked,
+      project: selectProject.value,
+      isdone: false,
+    };
+    arrayOfTasks.push(newTodo);
+    displayTasks(arrayOfTasks);
+    mainContainer.innerHTML = '';
+  });
+
+  console.log(document.querySelectorAll('#delete-modal'));
+
+  document.querySelectorAll('#delete-modal').forEach(item => {
+    item.addEventListener('click', () => {
+      mainContainer.innerHTML = '';
+    });
+  });
 });
 
-// add the project to the options
-
-const arrayOption = [];
-
-
+const newProjectBtn = document.getElementById('newProjectBtn');
+newProjectBtn.addEventListener('click', () => {
+  const mainContainer = document.getElementById('modalContainer');
+  mainContainer.innerHTML += `${projectForm}`;
+  const submitProjectbtn = document.getElementById('submit-project-form');
+  submitProjectbtn.addEventListener('click', (e) => {
+    const projectTitle = document.getElementById('projectTitle').value.toLowerCase();
+    if (!arrayProject.includes(projectTitle)) {
+      arrayProject.push(projectTitle);
+    }
+    console.log(arrayProject);
+    mainContainer.innerHTML = '';
+  });
+  document.querySelectorAll('#delete-modal').forEach(item => {
+    item.addEventListener('click', () => {
+      mainContainer.innerHTML = '';
+    });
+  });
+});
