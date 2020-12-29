9822e09731275a61b3e421c0245169ddf2aae727
brugobi <brgobbi@hotmail.com>
2020-12-29 13:13:58 -0500

    add test to fech TodoArray

--- src/__tests__/pureFunctions.test.js
+++ src/__tests__/pureFunctions.test.js
@@ -8,3 +8,24 @@ import {
   lastId,
 } from '../pureFunctions';
 
+test('fetch TodoArray', () => {
+  const todoArray = localStorage.setItem('arrayOfTodos', JSON.stringify([{
+    title: 'I have priority', description: 'Lorem', duedate: '2020-11-23T01:00:00.000', priority: true, project: 'hello', isDone: false, id: 1,
+  }, {
+    title: 'I have priority', description: 'ipsum', duedate: '2020-11-26T16:00:00.000', priority: true, project: 'hello', isDone: false, id: 2,
+    }]));
+  
+  expect(fetchTodoArrayFromLocalStorage()).toHaveLength(2);
+  expect(Array.isArray(fetchTodoArrayFromLocalStorage())).toBe(true);
+  expect(fetchTodoArrayFromLocalStorage()).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({ id: 1 }),
+      expect.objectContaining({ title: 'I have priority' }),
+      expect.objectContaining({ priority: true }),
+      expect.objectContaining({ project: 'hello' }),
+      expect.objectContaining({ isDone: false })
+    ])
+  )
+
+});
+
